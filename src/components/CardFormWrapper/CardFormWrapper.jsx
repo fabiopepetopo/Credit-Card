936f73d79dd4f4c2e2ffd8eb3d4b37ffd6defbd7
Fabio <fabio@oyga.me>
2022-05-30 21:22:50 -0300

    Responsive fix

--- src/components/CardFormWrapper/CardFormWrapper.jsx
+++ src/components/CardFormWrapper/CardFormWrapper.jsx
@@ -1,3 +1,4 @@
+import "./card-form.scss";
 import { Card } from "../Card";
 import { PaymentForm } from "../PaymentForm";
 import { useContext } from "react";
@@ -7,9 +8,9 @@ export const CardFormWrapper = () => {
   const { cardFormValues, brand } = useContext(CardContext);
 
   return (
-    <>
+    <div className={"cardForm"}>
       <Card brand={brand} cardFormValues={cardFormValues} />
       <PaymentForm />
-    </>
+    </div>
   );
 };
